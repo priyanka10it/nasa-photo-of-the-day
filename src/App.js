4b875a4e6f1ceec26f0ad14799190084e1300cb5
priyanka10it <priyanka10.it@gmail.com>
2020-05-26 23:52:50 -0700

    MVP Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import "./App.css"
-import {useState, useEffect} from 'react'
 import axios from "axios"
 
-import Header from "./components/header/header.js"
+import Header from "./components/header/Header.js"
 import PostContainer from "./components/PostContainer/PostContainer.js"
 
 
 
-const apiKey= "nBc60CjQCp0NmTe5dbp3rcreCA2T0kHStRg2sJGP"
+
 
 function App() {
   const [info, setInfo] = useState({});
@@ -16,14 +15,14 @@ function App() {
 
 
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?${apiKey}`)
-          .then(res => {
-            console.log(res)
-            setInfo(res.data)
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=nBc60CjQCp0NmTe5dbp3rcreCA2T0kHStRg2sJGP")
+          .then(response => {
+            console.log(response)
+            setInfo(response.data)
            
           })
-          .catch(err => {
-            debugger
+          .catch(error => {
+            console.log("Error!", error)
           })
         }, []);
   
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,12 @@ const Header = () => {
     return (
         <header className="header-section">
             <h1>Nasa Photo of The Day</h1>
-
+            <nav id="nav">
+                <a href="#">HOME</a>
+                <a href="#">ABOUT</a>
+                <a href="#">CONTACT US</a>
+                <a href="#">SIGN-UP</a>
+            </nav>
         </header>
     )
 }
